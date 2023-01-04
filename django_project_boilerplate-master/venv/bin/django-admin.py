c8f848c2b67b9d3b7b432b6dcc50fffa56fd0ed8
Kamil-Krawiec <kamil@Kamils-MacBook-Pro.local>
2023-01-04 14:56:56 +0100

    ups

--- django_project_boilerplate-master/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/kamil/Desktop/V sem/PO/POProject/django_project_boilerplate-master/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
