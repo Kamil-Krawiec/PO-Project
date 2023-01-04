a90bd39c89de0cac40c9c8f1a3d96f34b3b37306
Kamil-Krawiec <kamil@Kamils-MacBook-Pro.local>
2023-01-04 15:35:42 +0100

    first good look

--- django_project_boilerplate-master/PO/urls.py
+++ django_project_boilerplate-master/PO/urls.py
@@ -2,10 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from core.views import product_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('',product_list, name='product-list')
 ]
 
 if settings.DEBUG:
--- django_project_boilerplate-master/core/views.py
+++ django_project_boilerplate-master/core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def product_list(request):
+    context = {
+        'items': Product.objects.all()
+    }
+
+    return render(request,"product_list.html",context)
