0b8af09a0752e334e643b93960a1a1d28d074ef4
Kamil-Krawiec <kamil@Kamils-MacBook-Pro.local>
2023-01-15 13:40:32 +0100

    image and description added, single item view in progress

--- django_project_boilerplate-master/PO/settings.py
+++ django_project_boilerplate-master/PO/settings.py
@@ -7,7 +7,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
 ALLOWED_HOSTS = []
 
-
+MEDIA_URL = "/media/"
+MEDIA_ROOT=os.path.join(BASE_DIR,"media/")
 
 
 INSTALLED_APPS = [
